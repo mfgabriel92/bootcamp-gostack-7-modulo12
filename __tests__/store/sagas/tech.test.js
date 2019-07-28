00527bf444cddf4ee099b534c191224cb9629294
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-07-28 11:44:51 -0300

    Mock do axios

--- __tests__/store/sagas/tech.test.js
+++ __tests__/store/sagas/tech.test.js
@@ -1,12 +1,27 @@
 import { runSaga } from 'redux-saga'
-import { getTechsSuccess } from '../../../src/store/tech/actions'
+import ApiMock from 'axios-mock-adapter'
+import api from '../../../src/services/api'
+import { getTechsSuccess, getTechsFailure } from '../../../src/store/tech/actions'
 import { getTechs } from '../../../src/store/tech/sagas'
+
+const apiMock = new ApiMock(api)
 
 describe('Techs saga', () => {
   it('should be able to fetch the techs', async () => {
+    apiMock.onGet('techs').reply(200, ['Node.js'])
+
     const dispatch = jest.fn()
     await runSaga({ dispatch }, getTechs).toPromise()
 
     expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['Node.js']))
+  })
+
+  it('should fail when api returns an error', async () => {
+    apiMock.onGet('techs').reply(500)
+
+    const dispatch = jest.fn()
+    await runSaga({ dispatch }, getTechs).toPromise()
+
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure())
   })
 })
--- src/store/tech/sagas.js
+++ src/store/tech/sagas.js
@@ -5,7 +5,7 @@ import { getTechsSuccess, getTechsFailure } from '../../store/tech/actions'
 export function* getTechs() {
   try {
     const { data } = yield call(api.get, 'techs')
-    yield put(getTechsSuccess(data.techs))
+    yield put(getTechsSuccess(data))
   } catch (e) {
     yield put(getTechsFailure())
   }
