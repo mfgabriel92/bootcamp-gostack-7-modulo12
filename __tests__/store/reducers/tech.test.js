bd8204e4ad57a1783d480c354f2d4a7eb8daa302
Gabriel Monteiro Fernandes <gamofe@protonmail.ch>
2019-07-28 18:41:49 -0300

    Coverage report

--- __tests__/store/reducers/tech.test.js
+++ __tests__/store/reducers/tech.test.js
@@ -2,6 +2,11 @@ import reducer, { INITIAL_STATE } from '../../../src/store/tech/reducer'
 import { addTech } from '../../../src/store/tech/actions'
 
 describe('Techs reducer', () => {
+  it('DEFAULT', () => {
+    const state = reducer(undefined, {})
+    expect(state).toStrictEqual(INITIAL_STATE)
+  })
+
   it('tech/ADD_TECH', () => {
     const state = reducer(INITIAL_STATE, addTech('Node.js'))
     expect(state).toStrictEqual(['Node.js'])
